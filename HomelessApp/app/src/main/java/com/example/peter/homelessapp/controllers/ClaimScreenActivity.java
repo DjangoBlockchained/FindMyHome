a66c63703aa1eb052c8fd904674baf777b53a141
Valentine Wilson <valentineawilson@gmail.com>
2018-03-11 19:39:23 -0400

    added claim button and screen

---
+++ HomelessApp/app/src/main/java/com/example/peter/homelessapp/controllers/ClaimScreenActivity.java
@@ -0,0 +1,22 @@
+package com.example.peter.homelessapp.controllers;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.peter.homelessapp.R;
+
+/**
+ * Created by valentinewilson on 3/11/18.
+ */
+
+public class ClaimScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_claim_screen);
+
+    }
+}
--- HomelessApp/app/src/main/java/com/example/peter/homelessapp/controllers/ShelterDetailActivity.java
+++ HomelessApp/app/src/main/java/com/example/peter/homelessapp/controllers/ShelterDetailActivity.java
@@ -1,11 +1,13 @@
 package com.example.peter.homelessapp.controllers;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
-
+import android.widget.Button;
 import com.example.peter.homelessapp.R;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -28,19 +30,25 @@ public class ShelterDetailActivity extends AppCompatActivity{
     private String searchName;
     private String searchAge;
     private String searchGender;
+    private Button claimbutton;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shelter_details);
         list = findViewById(R.id.shelterdetails);
+        claimbutton = (Button) findViewById(R.id.claimbutton);
         adapter = new ArrayAdapter(ShelterDetailActivity.this, android.R.layout.simple_list_item_1, details);
         String name = getIntent().getStringExtra("Shelter Name");
         shelterRef = FirebaseDatabase.getInstance().getReference().child("shelters").child(name);
+        claimbutton.setOnClickListener((view) -> {
+            Intent intent = new Intent(ShelterDetailActivity.this, ClaimScreenActivity.class);
+            startActivity(intent);
+        });
         shelterRef.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                String string = (String) dataSnapshot.getValue();
-                details.add(dataSnapshot.getKey()+ ": "+ string);
+                Object data =  dataSnapshot.getValue();
+                details.add(dataSnapshot.getKey()+ ": "+ data);
                 adapter.notifyDataSetChanged();
             }
 
