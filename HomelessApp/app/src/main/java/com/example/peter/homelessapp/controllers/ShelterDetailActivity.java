9da85f92cc4c72a3c4dd0e1baad4aae5e95fc62b
pzupke3 <pzupke3@gatech.edu>
2018-03-27 10:46:47 -0400

    Let Peter pull

--- HomelessApp/app/src/main/java/com/example/peter/homelessapp/controllers/ShelterDetailActivity.java
+++ HomelessApp/app/src/main/java/com/example/peter/homelessapp/controllers/ShelterDetailActivity.java
@@ -1,12 +1,15 @@
 package com.example.peter.homelessapp.controllers;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
-
+import android.widget.Button;
 import com.example.peter.homelessapp.R;
+import com.example.peter.homelessapp.model.HomelessUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -28,24 +31,54 @@ public class ShelterDetailActivity extends AppCompatActivity{
     private String searchName;
     private String searchAge;
     private String searchGender;
+    private Button claimbutton;
+    //private HomelessUser currentUser;
+    private String username;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shelter_details);
         list = findViewById(R.id.shelterdetails);
+        claimbutton = (Button) findViewById(R.id.claimbutton);
         adapter = new ArrayAdapter(ShelterDetailActivity.this, android.R.layout.simple_list_item_1, details);
+        username = getIntent().getStringExtra("username");
         String name = getIntent().getStringExtra("Shelter Name");
         shelterRef = FirebaseDatabase.getInstance().getReference().child("shelters").child(name);
+        claimbutton.setOnClickListener((view) -> {
+            Intent intent = new Intent(ShelterDetailActivity.this, ClaimScreenActivity.class);
+            intent.putExtra("username", username);
+            intent.putExtra("Shelter Name", name);
+            startActivity(intent);
+        });
         shelterRef.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                String string = (String) dataSnapshot.getValue();
-                details.add(dataSnapshot.getKey()+ ": "+ string);
+
+                Object data =  dataSnapshot.getValue();
+
+                details.add(getLabel(dataSnapshot.getKey(), data));
                 adapter.notifyDataSetChanged();
             }
 
             @Override
             public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                Object data = dataSnapshot.getValue();
+                String key = dataSnapshot.getKey();
+                // Find the current row for the key and replace it with the new value.
+                int index = -1;
+                boolean found = false;
+                for (int i = 0; i < details.size() && !found; i++) {
+                    if (details.get(i).contains(key)) {
+                        index = i;
+                        found = true;
+                    }
+                }
+                if (index != -1) {
+                    details.set(index, getLabel(dataSnapshot.getKey(), data));
+                } else {
+                    details.add(getLabel(dataSnapshot.getKey(), data));
+                }
+                adapter.notifyDataSetChanged();
 
             }
 
@@ -65,5 +98,9 @@ public class ShelterDetailActivity extends AppCompatActivity{
             }
         });
         list.setAdapter(adapter);
+    }
+
+    private String getLabel(String key, Object value) {
+        return key + ": " + value;
     }
 }
